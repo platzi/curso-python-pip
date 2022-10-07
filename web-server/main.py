56968e9046d16cbe0b2c3822582903bb0360ce36
Nicolas Molina <hi@nicoytes.com>
2022-10-07 16:53:45 -0500

    save

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+    """
 
 def run():
     store.get_categories()
