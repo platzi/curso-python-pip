93f100f7fab8c57313029d8e3713a291df48895a
Nicolas Molina <hi@nicoytes.com>
2022-10-07 17:42:07 -0500

    volumes

--- app/charts.py
+++ app/charts.py
@@ -10,7 +10,7 @@ def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig('pie.png')
+  plt.savefig('chart_pie.png')
   plt.close()
 
 if __name__ == '__main__':
